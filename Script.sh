204bb8812aa2248e49599397608ba89f72d92d6f
mati <mati@gmail.com>
2024-02-19 16:55:19 -0500

    CAMBIOS

--- Script.sh
+++ Script.sh
@@ -2,18 +2,21 @@
 
 # Network testing script for git bash on Windows
 
+
 # Enseñar direccionamiento 
+read -p "Introduce IP" ip_address
 echo " Direccion Ip ingresada es : $ip_address" 
 echo "----------------------" 
 
 # Plantilla para presentar los resultados 
-echo "template= "
+
 echo "================= Resultados =============="
-echo " información del sistema : $system_info"
-echo "información de red: $network_info "
-echo " resultado de ping a $ip_address: $ping_result "
-echo "-----------------------------------"
+echo " información del sistema :"
+systeminfo
+echo "información de red: "
+arp -a 
+
 
 # mostrar resultados utilizando la platilla
-echo "$template"
-echo "--------------------"
+#"echo "$template"
+# echo "--------------------""
