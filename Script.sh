5e86ba8c392b5a64196ac4d703bb187afe8281c0
mati <mati@gmail.com>
2024-02-19 15:10:44 -0500

    Plnatilla de resultados

--- Script.sh
+++ Script.sh
@@ -6,4 +6,11 @@
 echo " Direccion Ip ingresada es : $ip_address" 
 echo "----------------------" 
 
+# Plantilla para presentar los resultados 
+echo "template= "
+echo "================= Resultados =============="
+echo " información del sistema : $system_info"
+echo "información de red: $network_info "
+echo " resultado de ping a $ip_address: $ping_result "
+echo "-----------------------------------"
 
