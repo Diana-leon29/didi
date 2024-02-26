80db0c1a3b5c81624c1644a303443731887c9ae8
matias <matias>
2024-02-26 11:23:02 -0500

    lorem

--- script-actividad.sh
+++ script-actividad.sh
@@ -2,6 +2,7 @@ class A {
    constructor() {
          this.foo= null;
          this.bar= null;
+         this.lorem= null;
          
     }
 }   
@@ -10,6 +11,8 @@ class A {
 let objeto_a= new A();
 objeto_a.foo= "nuevo";
 objeto_a.bar= 25;
+objeto_a.lorem= "Lorem ipsum dolor sit amet.";
 
-console.log("valor de foo:", objeto_a.foo)
+console.log("valor de foo:", objeto_a.foo);
 console.log("valor de bar:", objeto_a.bar);
+console.log("valor de lorem:", objeto_a.lorem);
