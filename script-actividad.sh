094a7f6292bb2eca1ee7cfd60c48ea1e47941d27
matias <matias>
2024-02-26 11:00:39 -0500

    Clase B

--- script-actividad.sh
+++ script-actividad.sh
@@ -13,3 +13,19 @@ objeto_a.bar= 25;
 
 console.log("valor de foo:", objeto_a.foo)
 console.log("valor de bar:", objeto_a.bar);
+
+
+
+class B {
+   constructor(foo, bar) {
+         this.foo= foo;
+         this.bar= bar;
+         
+    }
+}   
+
+//Ejemplo: 
+let objeto_b= new B("hello", 42);
+
+console.log("valor de foo:", objeto_b.foo);
+console.log("valor de bar:", objeto_b.bar);
